6d171acb774bfc8a4f777536c30577247c160dc8
Meagan Moore <816moore@gmail.com>
2020-12-09 09:15:20 -0500

    redux architecture

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux';
+
+export const songsReducer = () => {
+	return [
+		{
+			title: 'Gold', 
+			duration: '3:45',
+		},
+		{
+			title: 'Dissolve',
+			duration: '3:56',
+		},
+		{
+			title: 'Sweet Hibiscus Tea',
+			duration: '2:50',
+		},
+		{
+			title: 'Outlaw Baby',
+			duration: '3:50',
+		},
+	]
+}
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload
+	}
+	return selectedSong;
+}
+
+export default combineReducers({
+	songs: songsReducer,
+	selectedSong: selectedSongReducer,
+});
